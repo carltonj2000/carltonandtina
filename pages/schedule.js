2f3b5b48f8981db14321c5e803d2606a886efd4a
Carlton Joseph <carlton.joseph@gmail.com>
2018-01-21 21:09:00 -0800

    fix schedule table

--- pages/schedule.js
+++ pages/schedule.js
@@ -10,79 +10,81 @@ export default props => (
         </h3>
       </div>
       <table>
-        <tr>
-          <th>Time</th>
-          <th>Event</th>
-        </tr>
-        <tr>
-          <td>11:15 AM</td>
-          <td>
-            <Link href="/directions">Flamingo Hotel - Arrive &amp; Park</Link>
-          </td>
-        </tr>
-        <tr>
-          <td>11:30 AM</td>
-          <td>
-            Flamingo Garden - Guest Self Tour &amp; Photos<br />
-            Tina arrives before guests
-          </td>
-        </tr>
-        <tr>
-          <td>11:40 AM</td>
-          <td>
-            Chapel - Guest seat themselves in outside chapel garden venue<br />
-            Carlton arrives, speaks to chapel, then joins guests.
-          </td>
-        </tr>
-        <tr>
-          <td>12:00 PM</td>
-          <td>Chapel - Cermony Starts</td>
-        </tr>
-        <tr>
-          <td>12:30 PM</td>
-          <td>Chapel - Cermony Ends - Photos Starts</td>
-        </tr>
-        <tr>
-          <td>12:45 PM</td>
-          <td>Chapel - Depart Grounds</td>
-        </tr>
-        <tr>
-          <td>1:00 PM</td>
-          <td>
-            <Link href="/directions">Drive - To Lake LV</Link>
-          </td>
-        </tr>
-        <tr>
-          <td>1:45 PM</td>
-          <td>
-            <a href="https://goo.gl/maps/Nv6sCSpg1312">Mountain Lake Park</a> -
-            Break spot if needed.
-          </td>
-        </tr>
-        <tr>
-          <td>2:30 PM</td>
-          <td>Lake - Family Photos</td>
-        </tr>
-        <tr>
-          <td>3:00 PM</td>
-          <td>Lake - Tina &amp; Carlton's Photos</td>
-        </tr>
-        <tr>
-          <td>4:00 PM</td>
-          <td>Mimi's - Group Photos</td>
-        </tr>
-        <tr>
-          <td>4:30 PM</td>
-          <td>Mimi's - Dinner Start</td>
-        </tr>
-        <tr>
-          <td>6:30 PM</td>
-          <td>Mimi's - Dinner End</td>
-        </tr>
-        <tr>
-          <td>8:00 PM</td>
-          <td>Drinks - Tina &amp; Carlton's Home</td>
-        </tr>
+        <tbody>
+          <tr>
+            <th>Time</th>
+            <th>Event</th>
+          </tr>
+          <tr>
+            <td>11:15 AM</td>
+            <td>
+              <Link href="/directions">Flamingo Hotel - Arrive &amp; Park</Link>
+            </td>
+          </tr>
+          <tr>
+            <td>11:30 AM</td>
+            <td>
+              Flamingo Garden - Guest Self Tour &amp; Photos<br />
+              Tina arrives before guests
+            </td>
+          </tr>
+          <tr>
+            <td>11:40 AM</td>
+            <td>
+              Chapel - Guest seat themselves in outside chapel garden venue<br />
+              Carlton arrives, speaks to chapel, then joins guests.
+            </td>
+          </tr>
+          <tr>
+            <td>12:00 PM</td>
+            <td>Chapel - Cermony Starts</td>
+          </tr>
+          <tr>
+            <td>12:30 PM</td>
+            <td>Chapel - Cermony Ends - Photos Starts</td>
+          </tr>
+          <tr>
+            <td>12:45 PM</td>
+            <td>Chapel - Depart Grounds</td>
+          </tr>
+          <tr>
+            <td>1:00 PM</td>
+            <td>
+              <Link href="/directions">Drive - To Lake LV</Link>
+            </td>
+          </tr>
+          <tr>
+            <td>1:45 PM</td>
+            <td>
+              <a href="https://goo.gl/maps/Nv6sCSpg1312">Mountain Lake Park</a>{" "}
+              - Break spot if needed.
+            </td>
+          </tr>
+          <tr>
+            <td>2:30 PM</td>
+            <td>Lake - Family Photos</td>
+          </tr>
+          <tr>
+            <td>3:00 PM</td>
+            <td>Lake - Tina &amp; Carlton's Photos</td>
+          </tr>
+          <tr>
+            <td>4:00 PM</td>
+            <td>Mimi's - Group Photos</td>
+          </tr>
+          <tr>
+            <td>4:30 PM</td>
+            <td>Mimi's - Dinner Start</td>
+          </tr>
+          <tr>
+            <td>6:30 PM</td>
+            <td>Mimi's - Dinner End</td>
+          </tr>
+          <tr>
+            <td>8:00 PM</td>
+            <td>Drinks - Tina &amp; Carlton's Home</td>
+          </tr>
+        </tbody>
       </table>
       <style jsx>{`
         .container {
